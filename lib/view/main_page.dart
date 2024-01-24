46ef67c87c4850fcb97be5e930f883acd644e84b
seedri <serikawa.ed@gmail.com>
2024-01-24 16:03:21 +0900

    add-inputForm
    テキスト入力フォームを設置
    「検索」ボタンを押すと、下に入力内容が表示される

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -11,39 +11,33 @@ class MyHomePage extends ConsumerStatefulWidget {
 }
 
 class _MyHomePageState extends ConsumerState<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  String text = '';
+  String showText = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(onChanged: (_text) {
+              text = _text;
+            }),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showText = text;
+                  });
+                },
+                child: Text('検索')),
+            Text(showText)
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ));
+      ),
+    );
   }
 }
